c8af1fd30f90bbd50b206325bf479943f516602e
Patrick Monkelban <pmonkelban@gmail.com>
2016-05-25 11:22:41 -0400

    Clean Up

---
+++ 000/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * <num> - <name>
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ 1124/main.cpp
@@ -0,0 +1,18 @@
+//
+// 1124 - Celebrity jeopardy
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+
+    string s;
+
+    while (getline(cin, s))  {
+        cout << s << endl;
+    }
+}
+
+
